afe96bb349dd18d1646695fb1ce74b19f45293fe
Aliaksei <yarashsei@gmail.com>
2025-03-14 11:58:43 +0300

    Complete

--- FirstCheckPoint/MyQueue/MyQueue-main.cpp
+++ FirstCheckPoint/MyQueue/MyQueue-main.cpp
@@ -2,5 +2,21 @@
 #include "myqueue.h"
 
 int main() {
+        try {
+                myq<int> A;
+                myq<int> B;
+                A.push(1);
+                A.push(2);
+                A.push(5);
+                A.show();
+                A.emplace(2, 3);
+                std::cout << A.size() << std::endl;
+                A.show();
+                A.emplace(3, 4);
+                A.show();
+        }
+        catch (char const *err) {
+                std::cout << err << std::endl;
+        }
         return 0;
 }
