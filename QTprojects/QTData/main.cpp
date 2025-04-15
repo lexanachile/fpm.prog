686a2dcc579fd852f62f3106bbfb427dd561ba3c
Aliaksei <yarashsei@gmail.com>
2025-04-15 22:00:18 +0300

    regexdone

--- QTprojects/QTData/main.cpp
+++ QTprojects/QTData/main.cpp
@@ -1,19 +1,45 @@
 #include <QCoreApplication>
+#include <QRegularExpression>
+#include <QString>
+#include <QDebug>
+#include <QFile>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
+    setlocale(LC_ALL, "russian");
     QCoreApplication a(argc, argv);
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
+    QString path = "data.txt";
+    QFile file(path);
 
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
+    QTextStream out(stdout);
 
+    if(!file.open(QIODevice::ReadWrite)){
+        out << "11";
+    }
+    QTextStream ff(&file);
+
+    QString dataString = ff.readAll(); //"29.02.2025";
+    dataString.resize(10);
+
+    ff.seek(0);
+    ff << dataString;
+    out << "Before writing: " << dataString << Qt::endl;
+
+    QRegularExpression reg("((0[1-9]|1[0-9]|2[0-8])\\.02\\.2025)");
+
+    ff << Qt::endl;
+    if (reg.match(dataString).hasMatch())
+    {
+        out << dataString << " is a date of February 2025." << Qt::endl;
+        ff << dataString << " is a date of February 2025." << Qt::endl;
+    }
+    else
+    {
+        out << dataString << " is NOT a date of February 2025." << Qt::endl;
+        ff << dataString << " is NOT a date of February 2025." << Qt::endl;
+    }
+    file.close();
     return a.exec();
 }
